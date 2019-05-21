2b70c702b52762ffd824beaacab2f10e559c12c7
Pauline <pbaker@hotmail.co.uk>
2019-05-21 19:00:03 +0100

    added onclick functions to take you from the login activity to the create-user activity

---
+++ app/src/main/java/com/example/smack/CreateUserActivity.kt
@@ -0,0 +1,26 @@
+package com.example.smack
+
+import android.content.Intent
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+
+class CreateUserActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_create_user)
+    }
+
+    fun generateUserAvatar(view: View) {
+
+    }
+
+    fun generateBackgroundColourClicked(view: View) {
+
+    }
+
+    fun createCreateUserBtnClicked(view: View) {
+
+    }
+}
