eecd3496bb956e3ac8c9b5d0226bec34dc85aee9
Pauline <pbaker@hotmail.co.uk>
2019-06-26 14:52:00 +0100

    Added a spinner to the createUserActivity, and error toasts for if creating a user is unsuccessful, and created a Local Broadcast which will be sent out when creating a User is successful.

--- app/src/main/java/com/example/smack/Controller/CreateUserActivity.kt
+++ app/src/main/java/com/example/smack/Controller/CreateUserActivity.kt
@@ -1,12 +1,16 @@
 package com.example.smack.Controller
 
+import android.content.Intent
 import android.graphics.Color
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v4.content.LocalBroadcastManager
 import android.view.View
+import android.widget.Toast
 import com.example.smack.R
 import com.example.smack.Services.AuthService
 import com.example.smack.Services.UserDataService
+import com.example.smack.Utilities.BROADCAST_USER_DATA_CHANGE
 import kotlinx.android.synthetic.main.activity_create_user.*
 import java.util.*
 
@@ -18,6 +22,7 @@ class CreateUserActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_create_user)
+        createSpinner.visibility = View.INVISIBLE
     }
 
     fun generateUserAvatar(view: View) {
@@ -50,27 +55,62 @@ class CreateUserActivity : AppCompatActivity() {
     }
 
     fun createCreateUserBtnClicked(view: View) {
-
+        enableSpinner(true)
         val userName = createUserNameTxt.text.toString()
         val email = createEmailTxt.text.toString()
         val password = createPasswordTxt.text.toString()
 
-        AuthService.registerUser(this, email, password) { registerSuccess ->
-            if (registerSuccess) {
-                AuthService.loginUser(this, email, password) { loginSuccess ->
-                    if (loginSuccess) {
-                        AuthService.createUser(this, userName, email, userAvatar, avatarColor) {createSuccess ->
-                            if (createSuccess) {
-                                println(UserDataService.avatarName)
-                                println(UserDataService.avatarColor)
-                                println(UserDataService.name)
-                                finish()
+        if (userName.isNotEmpty() && email.isNotEmpty() && password.isNotEmpty()) {
+            AuthService.registerUser(this, email, password) { registerSuccess ->
+                if (registerSuccess) {
+                    AuthService.loginUser(this, email, password) { loginSuccess ->
+                        if (loginSuccess) {
+                            AuthService.createUser(this, userName, email, userAvatar, avatarColor) {createSuccess ->
+                                if (createSuccess) {
+                                    val userDataChange = Intent(BROADCAST_USER_DATA_CHANGE)
+                                    LocalBroadcastManager.getInstance(this).sendBroadcast(userDataChange)
+                                    enableSpinner(false)
+                                    finish()
+                                } else {
+                                    errorToast()
+                                }
                             }
+                        } else {
+                            errorToast()
                         }
                     }
+                } else {
+                    errorToast()
                 }
             }
+        } else {
+            Toast.makeText(this, "Make sure user name, email and password are filled in.", Toast.LENGTH_LONG).show()
+            enableSpinner(false)
         }
     }
+
+    fun errorToast() {
+        Toast.makeText(this, "Something went wrong. Please try again.", Toast.LENGTH_LONG).show()
+        enableSpinner(false)
+    }
+
+    fun enableSpinner(enable: Boolean) {
+        if (enable) {
+            createSpinner.visibility = View.VISIBLE
+            createCreateUserBtn.isEnabled = false
+            createAvatarImageView.isEnabled = false
+            backgroundColorBtn.isEnabled = false
+        } else {
+            createSpinner.visibility = View.INVISIBLE
+            createCreateUserBtn.isEnabled = true
+            createAvatarImageView.isEnabled = true
+            backgroundColorBtn.isEnabled = true
+        }
+    }
+
+
+
+
+
 }
 
--- app/src/main/java/com/example/smack/Utilities/Constants.kt
+++ app/src/main/java/com/example/smack/Utilities/Constants.kt
@@ -4,3 +4,7 @@ const val BASE_URL = "https://smack-chitty-chat-app.herokuapp.com/v1/"
 const val URL_REGISTER = "${BASE_URL}account/register"
 const val URL_LOGIN = "${BASE_URL}account/login"
 const val URL_CREATE_USER ="${BASE_URL}user/add"
+
+// BROADCAST CONSTANTS
+
+const val BROADCAST_USER_DATA_CHANGE = "BROADCAST_USER_DATA_CHANGE"
